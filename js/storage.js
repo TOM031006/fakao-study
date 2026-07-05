23881932bffb5d2cbf5e80ca10ea793d668e23f1
TOM031006 <r6tb26c2rs@privaterelay.appleid.com>
2026-07-05 15:09:46 +0800

    update js/storage.js

--- js/storage.js
+++ js/storage.js
@@ -239,7 +239,8 @@ FK.storage = {
       window.FK_SEED_DATA?.wuda2025 || [],
       window.FK_SEED_DATA?.wuda2024 || [],
       window.FK_SEED_DATA?.civilQuestions || [],
-      window.FK_SEED_DATA?.civilCh1Cards || []
+      window.FK_SEED_DATA?.civilCh1Cards || [],
+      window.FK_SEED_DATA?.civilCh2Cards || []
     ];
     const custom = this.getCustomQuestions();
     const customIds = new Set(custom.map(q => q.id));
